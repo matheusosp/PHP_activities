67bd2f2e0048250b1193ea06642ed726f2afeb2a
Matheus <58671449+matheusosp@users.noreply.github.com>
2020-12-10 23:38:55 -0300

    Update php-sub e soma.php

--- php-sub e soma.php
+++ php-sub e soma.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title>sua mae</title>
+  <title></title>
   <meta charset="windows-1252">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
   </head>
